4fc006bfaee8645ea4d0a87ca3dc60ba70330ca2
Wintress Mac <DON'T CHANGE THIS>
2016-07-27 12:08:03 -0700

    asdas

--- src/introtojavaworkshop/SpeakAndSpell.java
+++ src/introtojavaworkshop/SpeakAndSpell.java
@@ -16,7 +16,7 @@ package introtojavaworkshop;
 
 		public static void main(String[] args) {
 			// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-speak("i will rule the world");
+speak("slow motion");
 			// 2. Catch the user's answer in a String
 
 			// 3. If the user spelled the word correctly, speak "correct"
